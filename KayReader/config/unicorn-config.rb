f3bb1fffe0d948912f4a8f0bdceb935643c66220
dev <dev@192.168.1.9>
2012-09-09 15:54:30 +0900

    1554

--- KayReader/config/unicorn-config.rb
+++ KayReader/config/unicorn-config.rb
@@ -21,7 +21,7 @@ worker_processes 4
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-working_directory "/users/dev/Documents/eclipseSpace/keita/KayReader" # available in 0.94.0+
+working_directory "/users/dev/git/KayReader/KayReader" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
@@ -32,13 +32,13 @@ listen 8080, :tcp_nopush => true
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/users/dev/Documents/eclipseSpace/keita/KayReader/tmp/unicorn.pid"
+pid "/users/dev/git/KayReader/KayReader/tmp/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/users/dev/Documents/eclipseSpace/keita/KayReader/log/unicorn.stderr.log"
-stdout_path "/users/dev/Documents/eclipseSpace/keita/KayReader/log/unicorn.stdout.log"
+stderr_path "/users/dev/git/KayReader/KayReader/log/unicorn.stderr.log"
+stdout_path "/users/dev/git/KayReader/KayReader/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
